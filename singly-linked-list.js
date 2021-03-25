c60bdc670d30cc74c276c48f45674926c2d1665f
jasperwreed <jmixon.reed@gmail.com>
2021-03-25 14:00:38 -0700

    insertAtEnd() added to LinkedList class

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -19,4 +19,39 @@ class LinkedList {
 
     return this.head
   }
+
+  insertAtEnd(data) {
+    let newNode = new Node(data)
+
+    if(!this.head) {
+      this.head = newNode
+      return this.head
+    }
+
+    let current = this.head
+    while(current.next !== null) {
+      current = current.next
+    }
+
+    current.next = newNode
+    return this.head
+  }
+
+  display() {
+    let node = this.head
+    while(node) {
+      console.log(node)
+      node = node.next
+    }
+  }
 }
+
+let list = new LinkedList()
+
+list.insertAtEnd(1)
+list.insertAtEnd(2)
+list.insertAtEnd(3)
+list.insertAtEnd(4)
+list.insertAtEnd(5)
+
+list.display()
